5441fd10b154772e3290e3b71d987c47b0bf4619
Prithvi Dev <prithvidevkanojia@gmail.com>
2020-09-20 15:13:25 +0530

    Upload

---
+++ Reverse of Linked list/print_in_reverse.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) {
+        while (node != null) {
+            System.out.print(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                System.out.print(sep);
+            }
+        }
+    }
+
+    // Complete the reversePrint function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static void reversePrint(SinglyLinkedListNode head) {
+        if(head == null){
+            return;
+        }
+        SinglyLinkedListNode curr = head;
+        Stack<SinglyLinkedListNode> ss = new Stack<SinglyLinkedListNode>();
+        while(curr != null){
+            ss.push(curr);
+            curr = curr.next;
+        }
+        while(!ss.isEmpty()){
+            SinglyLinkedListNode rev = ss.pop();
+            System.out.println(rev.data);
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
